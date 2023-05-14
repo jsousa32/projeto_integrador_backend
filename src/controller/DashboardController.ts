ed205852768226983f623deb20ef7724a965fdf6
João Lucas <joao-rulez@hotmail.com>
2023-05-14 18:29:11 -0300

    [fix]: refactor dashboard info

--- src/controller/DashboardController.ts
+++ src/controller/DashboardController.ts
@@ -1,12 +1,17 @@
 import { Request, Response } from "express";
 import { DashboardService } from "../service/DashboardService";
+import moment from "moment";
 
 export class DashboardController {
     constructor(private dashboardService: DashboardService) {}
 
     dashboard = async (request: Request, response: Response) => {
         try {
-            const dashboardInfo = await this.dashboardService.dashboardInfo();
+            const date = new Date();
+
+            const newDate = moment(date).format("YYYY-MM-DD");
+
+            const dashboardInfo = await this.dashboardService.dashboardInfo(newDate);
 
             return response.status(200).json(dashboardInfo);
         } catch (error) {
--- src/service/DashboardService.ts
+++ src/service/DashboardService.ts
@@ -1,13 +1,18 @@
+import { Op } from "sequelize";
 import { Appointment } from "../models/Appointment";
 import { Doctor } from "../models/Doctor";
 import { Medicine } from "../models/Medicine";
-import { Patient } from "../models/Patient";
-
 export class DashboardService {
-    async dashboardInfo() {
-        const countDoctor = await Doctor.count();
-        const countPatient = await Patient.count();
-        const countAppointments = await Appointment.count();
+    async dashboardInfo(date: string) {
+        const countAppointmentsAtDay = await Appointment.count({
+            include: [{ model: Doctor, attributes: ["speciality"] }],
+            where: { date: { [Op.like]: date } },
+        });
+        const countAppointments = await Appointment.count({
+            include: [{ model: Doctor, attributes: ["speciality"] }],
+            where: { date: { [Op.like]: date } },
+            group: ["Doctor.speciality"],
+        });
         const allMedicines = await Medicine.findAll({
             limit: 5,
             attributes: ["name", "manufacturer", "quantity", "packaging"],
@@ -15,8 +20,7 @@ export class DashboardService {
         });
 
         return {
-            numberDoctor: countDoctor,
-            numberPatient: countPatient,
+            numberAppointmentsAtDay: countAppointmentsAtDay,
             numberAppointments: countAppointments,
             medicine: allMedicines,
         };
