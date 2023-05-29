e703d214738b888e69f5e426e6fe5d685ba531fd
João Lucas <joao-rulez@hotmail.com>
2023-05-29 20:57:19 -0300

    [fix]: config to render

--- src/database/dbConnection.ts
+++ src/database/dbConnection.ts
@@ -6,6 +6,7 @@ dotenv.config();
 export const db = new Sequelize({
     database: process.env.DB_DATABASE,
     username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT as unknown as number,
     dialect: process.env.DB_DIALECT as Dialect,
